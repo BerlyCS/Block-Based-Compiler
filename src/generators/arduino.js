22581e198e939bf3ec7f3abca1c93bceb1b01e98
BerlyCS <>
2025-06-22 19:42:51 -0500

    deleted files

--- src/generators/arduino.js
+++
@@ -1,5 +0,0 @@
-/* This file is part of the BlocklyDuino project, which is released under the GNU General Public License (GPL) version 3.*/
-
-sampleGenerator.forBlock['sample_block'] = function(block, generator) {
-  return 'my code string';
-};
